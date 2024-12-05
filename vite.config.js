152d8c2a5ef3d2221d3dbfb29cbecbf509346cdd
Angus MacMinn <angus13@hotmail.ca>
2024-12-05 10:03:21 -0800

    more package updates

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import glsl from 'vite-plugin-glsl'
 
 export default {
-  root: 'src/',
-  publicDir: '../static/',
-  base: './',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    sourcemap: true
-  },
-  plugins: [
-    glsl()
-  ]
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server: {
+        host: '0.0.0.0',
+        strictPort: true,
+        port: Number(process.env.PORT) || 3000
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+    plugins: [glsl()]
 }
